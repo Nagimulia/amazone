e6f444d85756ed65dc683bda26ea47d44f51c0f0
Keneuova Nagima <nagimakeneuova@gmail.com>
2023-12-06 00:44:07 +0600

    create helres and products page

---
+++ src/components/Product.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from 'react'
+import { ProductType } from '../../type';
+
+interface Item {
+	products: ProductType[]
+}
+
+const Product = ({products}:Item) => {
+
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default Product
