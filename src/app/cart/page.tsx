cd7659f59fd7f22fe2459123081be2ebdf4444ea
Keneuova Nagima <nagimakeneuova@gmail.com>
2023-12-06 17:15:37 +0600

    create pages

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from "@/components/Container";
+import Title from '@/components/Title';
+type Props = {}
+
+const page = (props: Props) => {
+	return (
+		<Container>
+			<Title title="Your cart"/>
+		</Container>
+	)
+}
+
+export default page
---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+import Title from "@/components/Title";
+
+type Props = {}
+
+const page = (props: Props) => {
+	return (
+		<Container>
+			<Title title="Your Wishlist"/>
+		</Container>
+	)
+}
+
+export default page
