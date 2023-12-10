6e8bf46f4657cc4bca5c74134a1731098cb91490
Keneuova Nagima <nagimakeneuova@gmail.com>
2023-12-10 17:54:02 +0600

    favorite items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,16 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { navigation } from "@/constants/data";
 import { signIn, useSession } from "next-auth/react";
-interface Props {}
+import { useSelector } from "react-redux";
+import { StateProps } from "../../type";
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const pathname = usePathname();
   const { data: session } = useSession();
+
+  const { productData, favoriteData } = useSelector(
+    (state: StateProps) => state.pro
+  );
 
   return (
     <div className="w-full h-20 border-b-[1px] border-zinc-500 bg-white text-zinc-600 sticky top-0 z-50 bg-white/80 backdrop-blur-2xl">
@@ -41,7 +46,7 @@ const Navbar = (props: Props) => {
           >
             <Heart className="w-7 h-7" />
             <span className="absolute top-0 -left-1 bg-zinc-800 text-zinc-200 w-4 h-4 rounded-full text-xs flex items-center justify-center group-hover:bg-black font-semibold group-hover:text-white">
-              4
+              {favoriteData ? favoriteData.length : 0}
             </span>
           </Link>
           <Link
@@ -50,25 +55,26 @@ const Navbar = (props: Props) => {
           >
             <ShoppingBagIcon className="w-7 h-7" />
             <span className="absolute top-0 -left-1 bg-zinc-800 text-zinc-200 w-4 h-4 rounded-full text-xs flex items-center justify-center group-hover:bg-black font-semibold group-hover:text-white">
-              8
+              {productData ? productData.length : 0}
             </span>
           </Link>
-					{
-						session ?
-          <Link href={'/profile'}
-            className="hover:text-black cursor-pointer duration-200 relative overflow-hidden group text-sm uppercase font-semibold"
-          >
-            Profile
-            <span className="absolute h-[1px] w-full bg-blue-700 left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-500" />
-          </Link> : 
-					<button
-					onClick={() => signIn()}
-					className="hover:text-black cursor-pointer duration-200 relative overflow-hidden group text-sm uppercase font-semibold"
-				>
-					Login
-					<span className="absolute h-[1px] w-full bg-blue-700 left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-500" />
-				</button>
-					}
+          {session ? (
+            <Link
+              href={"/profile"}
+              className="hover:text-black cursor-pointer duration-200 relative overflow-hidden group text-sm uppercase font-semibold"
+            >
+              Profile
+              <span className="absolute h-[1px] w-full bg-blue-700 left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-500" />
+            </Link>
+          ) : (
+            <button
+              onClick={() => signIn()}
+              className="hover:text-black cursor-pointer duration-200 relative overflow-hidden group text-sm uppercase font-semibold"
+            >
+              Login
+              <span className="absolute h-[1px] w-full bg-blue-700 left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-500" />
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
-import { ProductType } from "../../type";
+import { ProductType, StateProps } from "../../type";
 import Link from "next/link";
 import Image from "next/image";
 import { Heart } from "lucide-react";
 import FormatterPrice from "./FormatterPrice";
-import { useDispatch } from "react-redux";
-import { addToCart } from "@/redux/proSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, addToFavorite } from "@/redux/proSlice";
+import toast, { Toaster } from "react-hot-toast";
 
 interface Item {
   products: ProductType[];
@@ -14,6 +15,11 @@ interface Item {
 
 const Product = ({ products }: Item) => {
   const dispatch = useDispatch();
+  const { favoriteData } = useSelector((state: StateProps) => state.pro);
+  const isFavorite = (productId: any) => {
+    return favoriteData.some((favoriteItem) => favoriteItem._id === productId);
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mt-10">
       {products.map((item) => (
@@ -30,7 +36,18 @@ const Product = ({ products }: Item) => {
               className="w-full h-80 object-contain lg:object-cover group-hover:scale-105 duration-300 "
             />
           </Link>
-          <Heart className="absolute top-4 right-4 text-zinc-500 w-5 h-5 hover:text-black cursor-pointer duration-200" />
+          <Heart
+            fill={isFavorite(item._id) ? "red" : "black"}
+            onClick={() => {
+              dispatch(addToFavorite(item));
+              if (isFavorite(item?._id)) {
+                toast.error(`${item.title} removed from favorites`);
+              } else {
+                toast.success(`${item.title} added from favorites`);
+              }
+            }}
+            className="absolute top-4 right-4 text-zinc-500 w-5 h-5 hover:text-black cursor-pointer duration-200"
+          />
           <div className="p-4 bg-zinc-100 group-hover:bg-zinc-50 group-hover:shadow-xl duration-300">
             <p className="group-hover:text-designColor duration-300">
               {item?.title}
@@ -40,7 +57,10 @@ const Product = ({ products }: Item) => {
             </p>
             <div className="flex items-center justify-between text-sm mt-2">
               <button
-                onClick={() => dispatch(addToCart(item))}
+                onClick={() => {
+                  dispatch(addToCart(item)),
+                    toast.success(`${item?.title} is added to Cart`);
+                }}
                 className="uppercase font-semibold hover:text-designColor duration-300"
               >
                 Add to cart
@@ -55,6 +75,15 @@ const Product = ({ products }: Item) => {
           </div>
         </div>
       ))}
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            background: "#000",
+            color: "#fff",
+          },
+        }}
+      />
     </div>
   );
 };
