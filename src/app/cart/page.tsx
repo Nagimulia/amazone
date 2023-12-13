7ddc3fed5d58ae624ac5aa5941096390237c72c0
Keneuova Nagima <nagimakeneuova@gmail.com>
2023-12-13 15:36:20 +0600

    complited

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from "@/components/Container";
 import Title from '@/components/Title';
+import Cart from '@/components/Cart';
 type Props = {}
 
 const page = (props: Props) => {
 	return (
 		<Container>
 			<Title title="Your cart"/>
+			<Cart/>
 		</Container>
 	)
 }
